4059c68645662c86660715c73f08ab492173c448
Rita <mariarita.sartini@mail.polimi.it>
2026-09-24 17:41:54 +0200

    Implementazione
    Metodi add e prod.

--- codice/src/main/java/calculator/Calculator.java
+++ codice/src/main/java/calculator/Calculator.java
@@ -1,5 +1,16 @@
 package calculator;
 
 public class Calculator {
-
+	public Integer add(Integer n1, Integer n2) {
+		Integer n3;
+		n3= n1+n2;
+		return n3;
+	}
+	
+	public Integer prod(Integer n1, Integer n2) {
+		Integer n3;
+		n3=n1*n2;
+		return n3;
+	}
+	
 }
